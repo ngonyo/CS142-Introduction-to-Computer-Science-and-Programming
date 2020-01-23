8bff1d61e2dc7cf09e5360f19bdb0e8fe5abb251
Gonyo, Nathan <ngonyo@butler.edu>
2020-01-23 15:40:14 -0500

    Add files via upload

---
+++ 1_23_20.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+	int x;
+	std::cout << "Welcome to our program, please enter your favorite number" << "/n";
+	std::cin >> x << "/n";
+	std::cout << "x is now" << x << "/n";
+	
+	
