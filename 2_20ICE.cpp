0ea009c079415efff28178a7c406450662b75e43
Gonyo, Nathan <ngonyo@butler.edu>
2020-02-20 15:39:58 -0500

    Add files via upload

---
+++ 2_20ICE.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+
+int main()
+{
+int tacocount (0);
+std::cout << "Please Enter Amount of Tacos" << std::endl;
+std::cin >> tacocount;
+	while (tacocount > 0)
+	{
+		std::cout << "Taco" << std::endl;
+		tacocount=(tacocount-1);
+	}
+	
+std::string letter;	
+std::string favoriteLetter;
+favoriteLetter = "z";
+std::cout << "What is my favorite letter of the alphabet?" << std::endl;
+do 
+{	
+	std::cout << "Guess Again." << std::endl;
+	std::cin >> letter;
+} while (letter != favoriteLetter || );
+
+int number1 (0);
+std::cout << "Please Enter Number" << std::endl;
+std:cin >> number1;
+for (int x=number1, x>0, x--)
+	{	
+		std::cout << number1*number1-- <<std:end1;
+
+	}	
+return 0;
+}
+	
+	
