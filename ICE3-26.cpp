3d358295e73f69eab169ec68b12c51cea8672117
Gonyo, Nathan <ngonyo@butler.edu>
2020-03-26 15:56:57 -0500

    Add files via upload

---
+++ ICE3-26.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstdlib>
+
+void getInput(int & x, int & y);
+
+int main()
+{
+
+void getInput(int & x, int & y)
+{
+	std::cout << "Please enter one value: ";
+	std::cin >> x;
+	
+	std::cout << "Please enter a second value: ";
+	std::cin >> y;	
+}
