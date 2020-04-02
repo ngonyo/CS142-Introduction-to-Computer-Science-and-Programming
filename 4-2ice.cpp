4f31e50539334a2e01581f1f4a3d0c7f9ef66051
Gonyo, Nathan <ngonyo@butler.edu>
2020-04-02 14:47:01 -0500

    Add files via upload

---
+++ 4-2ice.cpp
@@ -0,0 +1,48 @@
+int main ()
+{
+	std::ifstream instream;
+	std::ofstream outStream;
+
+	int x(0);
+
+	std::cout << "enter favorite number" << std::endl;
+	std::cin >> x;
+
+	outStream.open("number1.txt");
+
+	outStream << x << std::endl;
+
+	return 0;
+}
+
+int main ()
+int x (0);
+int y (0);
+
+{
+	std::ifstream instream;
+	std::ofstream outStream;
+
+	
+
+	std::cout << "enter favorite number" << std::endl;
+	std::cin >> x;
+
+	inStream.open("ice4_2.txt")
+
+	inStream >> x;
+	inStream >> y;
+	inStream.close();
+
+	swap(x,y);
+
+	outsream.open("ice4_2new.txt");
+	outStream << x << std::endl;
+	outStream << y << std::endl;
+
+
+	return 0;
+}
+
+
+
